f6f046ce6d45197fa69ecd958d599aec1802cc11
yasminSarbaoui93 <yasmin.sarbaoui@gmail.com>
2024-09-12 12:37:56 +0200

    randomTerms file with random function working correctly

--- randomTerms.py
+++ randomTerms.py
@@ -1,14 +1,12 @@
 import random
-from telegram_bot import german_words
-import telebot
 
 #Function that gets the translation of a word taken from the dictionary
-def get_translation(word):
+def get_translation(word, german_words):
     translation = german_words.get(word)
     return translation
 
 #Function that checks if the user response is correct or not
-def check_response(message, translation):
+def check_response(message, translation, bot):
     print("User response: " + message.text)
     if message.text == translation:
         bot.reply_to(message, "Correct! ✅")
@@ -17,10 +15,10 @@ def check_response(message, translation):
 
 # Function that asks the user to translate a random word from the dictionary
 #@bot.message_handler(commands=['random'])
-def send_random_word(bot, message):
+def send_random_word(bot, message, german_words):
     random_word = random.choice(list(german_words.keys()))
-    translation = get_translation(random_word)
+    translation = get_translation(random_word, german_words)
     print("Random word: " + random_word)
     bot.reply_to(message, f"Translate this word: {random_word}")
     #bot.register_next_step_handler(message, check_response(translation))
-    bot.register_next_step_handler(message, lambda msg: check_response(msg, translation)) #Check better lambda function?
+    bot.register_next_step_handler(message, lambda msg: check_response(msg, translation, bot)) #Check better lambda function?
--- telegram_bot.py
+++ telegram_bot.py
@@ -2,7 +2,8 @@ import os
 import random
 import telebot
 from dotenv import load_dotenv
-from telegram_bot import send_random_word
+from randomTerms import send_random_word
+
 # Load the environment variables
 load_dotenv()
 API_TOKEN = os.getenv("API_TOKEN")
@@ -31,30 +32,10 @@ def send_info(message):
 def send_info(message):
     bot.reply_to(message, "Goodbye! Have a nice day")
 
-#Function that gets the translation of a word taken from the dictionary
-# def get_translation(word):
-#     translation = german_words.get(word)
-#     return translation
 
-#Function that checks if the user response is correct or not
-# def check_response(message, translation):
-#     print("User response: " + message.text)
-#     if message.text == translation:
-#         bot.reply_to(message, "Correct! ✅")
-#     else:
-#         bot.reply_to(message, "Incorrect! ❌")
-
-# Function that asks the user to translate a random word from the dictionary
 @bot.message_handler(commands=['random'])
-# def send_random_word(message):
-#     random_word = random.choice(list(german_words.keys()))
-#     translation = get_translation(random_word)
-#     print("Random word: " + random_word)
-#     bot.reply_to(message, f"Translate this word: {random_word}")
-#     #bot.register_next_step_handler(message, check_response(translation))
-#     bot.register_next_step_handler(message, lambda msg: check_response(msg, translation)) #Check better lambda function?
 def handle_random_word(message):
-    send_random_word(bot, message)
+    send_random_word(bot, message, german_words)
 
 # Message handler for all other messages    
 @bot.message_handler(func=lambda message: True)
