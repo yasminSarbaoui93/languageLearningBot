93b28a6fb577369e059e5e8f617b2d70f6daeaa6
yasminSarbaoui93 <yasmin.sarbaoui@gmail.com>
2024-09-23 11:57:35 +0200

    Creating two methods instead of if else

--- addWord.py
+++ addWord.py
@@ -9,27 +9,40 @@ german_term = ""
 # Function to add a new word to the dictionary, to be called from telegram_bot.py
 def add_word(message, bot):
     bot.reply_to(message, f"Type the english word you want to add to the dictionary")
-    bot_reply = bot.register_next_step_handler(message, lambda msg: register_word(msg, bot))
+    bot_reply = bot.register_next_step_handler(message, lambda msg: ask_for_original_word(msg, bot))
 
 # Function to register the word to be added to the dictionary
-def register_word(user_message, bot):
-    global english_term, german_term
-    # check if english term is empty, meaning it was not added by the user yet
-    if english_term == "":
-        english_term = user_message.text
-        bot.reply_to(
-            user_message, f"Type the German translation of the word {english_term}"
-        )
-        bot.register_next_step_handler(user_message, lambda msg: register_word(msg, bot))
-    else:
-        german_term = user_message.text
-        new_word = pd.DataFrame({"English": [english_term], "German": [german_term]})
-        english_term = ""
-        german_term = ""
-        save_word(new_word)
-        bot.reply_to(user_message, "The word has been added to the dictionary")
+def ask_for_original_word(user_message, bot):
+    global english_term  #, german_term
+    english_term = user_message.text
+    bot.reply_to(
+        user_message, f"Type the German translation of the word {english_term}"
+    )
+    bot.register_next_step_handler(user_message, lambda msg: ask_for_translated_word(msg, bot))
 
-# Function to save the new word to the csv file
+
+
+def ask_for_translated_word(user_message, bot):
+    global german_term
+    german_term = user_message.text
+    new_word = pd.DataFrame({"English": [english_term], "German": [german_term]})
+    english_term = ""
+    german_term = ""
+    save_word(new_word)
+    bot.reply_to(user_message, "The word has been added to the dictionary")
+
+
+
+
+
+
+
+
+
+
+
+
+# Function to save the new word to the csv
 def save_word(new_word):
     if os.path.exists("TermsList.csv"):
         terms_data = pd.read_csv("TermsList.csv", sep=";")
@@ -38,3 +51,28 @@ def save_word(new_word):
     else:
         terms_data = new_word
     terms_data.to_csv("TermsList.csv", sep=";", index=False)
+
+
+
+
+
+
+
+
+# # Function to register the word to be added to the dictionary
+# def ask_for_original_word(user_message, bot):
+#     global english_term, german_term
+#     # check if english term is empty, meaning it was not added by the user yet
+#     if english_term == "":
+#         english_term = user_message.text
+#         bot.reply_to(
+#             user_message, f"Type the German translation of the word {english_term}"
+#         )
+#         bot.register_next_step_handler(user_message, lambda msg: ask_for_original_word(msg, bot))
+#     else:
+#         german_term = user_message.text
+#         new_word = pd.DataFrame({"English": [english_term], "German": [german_term]})
+#         english_term = ""
+#         german_term = ""
+#         save_word(new_word)
+#         bot.reply_to(user_message, "The word has been added to the dictionary")
