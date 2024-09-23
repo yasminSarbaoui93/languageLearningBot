7e0d3a23307e935d9c930d481d298ca4fec2e3d9
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2024-09-23 12:24:58 +0200

    chore: Clean up method structure and var names

--- addWord.py
+++ addWord.py
@@ -6,36 +6,47 @@ english_term = ""
 german_term = ""
 
 # Function to add a new word to the dictionary, to be called from telegram_bot.py
-def add_word(message, bot):
-    bot.reply_to(message, f"Type the english word you want to add to the dictionary")
-    bot_reply = bot.register_next_step_handler(message, lambda msg: ask_for_original_word(msg, bot))
+def add_word(user_message, bot):
+    ask_for_original_word(user_message, bot)    
 
 # Function to register the word to be added to the dictionary
 def ask_for_original_word(user_message, bot):
-    global english_term  #, german_term
-    english_term = user_message.text
-    bot.reply_to(
-        user_message, f"Type the German translation of the word {english_term}"
-    )
-    bot.register_next_step_handler(user_message, lambda msg: ask_for_translated_word(msg, bot))
+    bot.reply_to(user_message, f"Type the english word you want to add to the dictionary")
+    bot.register_next_step_handler(user_message, lambda user_message: ask_for_translated_word(user_message, bot))
 
 # Function to register the translation of the word to be added to the dictionary
 def ask_for_translated_word(user_message, bot):
-    global english_term, german_term
-    german_term = user_message.text
-    new_word = pd.DataFrame({"English": [english_term], "German": [german_term]})
-    english_term = ""
-    german_term = ""
-    save_word(new_word)
-    bot.reply_to(user_message, "The word has been added to the dictionary")
+    # Save english term
+    global english_term
+    english_term = user_message.text
 
+    # Ask for the translation
+    bot.reply_to(
+        user_message, f"Type the German translation of the word {english_term}"
+    )
+    bot.register_next_step_handler(user_message, lambda user_message: save_word(user_message, bot))    
 
 # Function to save the new word to the csv
-def save_word(new_word):
+def save_word(user_message, bot):
+    # Save german term
+    global english_term, german_term
+    german_term = user_message.text
+
+    # Define and save full new word
+    new_word = pd.DataFrame({"English": [english_term], "German": [german_term]})
+    save_word_to_csv_file(new_word)
+
+    # Reset global variables
+    english_term = ""
+    german_term = ""
+        
+    bot.reply_to(user_message, "The word has been added to the dictionary")
+
+# Function to save the new word to the csv file
+def save_word_to_csv_file(new_word):
     if os.path.exists("TermsList.csv"):
         terms_data = pd.read_csv("TermsList.csv", sep=";")
         terms_data = pd.concat([terms_data, new_word], ignore_index=True)
-
     else:
         terms_data = new_word
     terms_data.to_csv("TermsList.csv", sep=";", index=False)
