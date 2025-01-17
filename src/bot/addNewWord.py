028054e58d50c291a4dfaca10ead84b38f5775d2
yasminSarbaoui93 <yasmin.sarbaoui@gmail.com>
2025-01-17 13:41:04 +0100

    chore: deleted userID from .env and conversation.py, cleaned up conversation.py

--- src/bot/addNewWord.py
+++ src/bot/addNewWord.py
@@ -2,8 +2,8 @@
 import os
 from src.repository.vocabulary import save_word
 
-nativelanguage_word = ""
-translation = ""
+base_language_word = ""
+learning_language_word = ""
 
 
 def add_word_to_dictionary(user_message, bot):
@@ -14,10 +14,10 @@ def add_word_to_dictionary(user_message, bot):
     user_message: the message object from the user
     bot: the bot object to send the message
     """
-    _ask_for_original_word(user_message, bot)    
+    _ask_for_base_language_word(user_message, bot)    
 
 
-def _ask_for_original_word(user_message, bot):
+def _ask_for_base_language_word(user_message, bot):
     """
     Function to ask the user for the word to be added to the dictionary
     
@@ -26,22 +26,22 @@ def _ask_for_original_word(user_message, bot):
     bot: the bot object to send the message
     """
     #bot.reply_to(user_message, f"Type the english word you want to add to the dictionary")
-    bot.send_message(user_message.chat.id, f"Type the english word you want to add to the dictionary")
-    bot.register_next_step_handler(user_message, lambda user_message: _ask_for_translated_word(user_message, bot))
+    bot.send_message(user_message.chat.id, f"Type the word you want to add to the dictionary in your <b>base language</b>", parse_mode='HTML')
+    bot.register_next_step_handler(user_message, lambda user_message: _ask_for_learninig_language_word(user_message, bot))
 
 
-def _ask_for_translated_word(user_message, bot):
+def _ask_for_learninig_language_word(user_message, bot):
     """
-    Function to ask the user for the translation of the word to be added to the dictionary
+    Function to ask the user for the translation in the larning language of the word to be added to the dictionary. This function I add because I prefer to put my own translation instead of the dictionary one (e.g. some might prefer using some dialect words instead of the official ones)
     
     args:
     user_message: the message object from the user
     bot: the bot object to send the message
     """
-    global nativelanguage_word
-    nativelanguage_word = user_message.text
-    #bot.reply_to(user_message, f"Type the German translation of the word {nativelanguage_word}")
-    bot.send_message(user_message.chat.id, f"Type the German translation of the word {nativelanguage_word}")
+    global base_language_word
+    base_language_word = user_message.text
+    #bot.reply_to(user_message, f"Type the German learning_language_word of the word {base_language_word}")
+    bot.send_message(user_message.chat.id, f"Type the translation of the word {base_language_word} in the <b>language you are learning</b>", parse_mode='HTML')
     bot.register_next_step_handler(user_message, lambda user_message: _save_word_to_db(user_message, bot))    
 
 
@@ -53,26 +53,14 @@ def _save_word_to_db(user_message, bot):
     user_message: the message object from the user
     bot: the bot object to send the message
     """
-    global nativelanguage_word, translation
-    translation = user_message.text
-    
+    global base_language_word, learning_language_word
+    learning_language_word = user_message.text
     try:
-        save_word(nativelanguage_word, translation, user_message.from_user.first_name, user_message.from_user.last_name, str(user_message.from_user.id), user_message.from_user.username)
-        bot_response = f"The word <b>{nativelanguage_word}</b> has been added to the dictionary"
+        save_word(str(user_message.from_user.id), base_language_word, learning_language_word)
+        bot_response = f"The word <b>{base_language_word}</b> = <b>{learning_language_word}</b> has been added to the dictionary"
     except Exception as e:
         print(f"An error occurred: {e}")
-        bot_response = f"Sorry, an error occurred while trying to save the word."
-
-
-
-    nativelanguage_word = ""
-    translation = ""
-    print(f"chat id= {user_message.chat.id}")
-    print(f"user id= {user_message.from_user.id}")
-    print(f"username= {user_message.from_user.username}")
-    print(f"name= {user_message.from_user.first_name}")
-    print(f"last name= {user_message.from_user.last_name}")
-    print(f"number= {user_message.from_user.phone_number}")
-
-
+        bot_response = e
+    base_language_word = ""
+    learning_language_word = ""
     bot.send_message(user_message.chat.id, bot_response, parse_mode='HTML')
