97474c8470436e0535fc15ba76ee1889741e07bc
yasminSarbaoui93 <yasmin.sarbaoui@gmail.com>
2025-01-17 12:32:50 +0100

    fix: made sure the system message is passed to AOAI

--- src/bot/conversation.py
+++ src/bot/conversation.py
@@ -25,10 +25,10 @@ def initializeConversation(message, bot, newConversation):
         german_words.append(str(all_words[i][1]))
     german_words = str(german_words)
     print(f"Dictionary from user {message.from_user.first_name} and dictionary is: containing {len(all_words)} words")
-    _manageConversation(message, bot, newConversation)
+    _manageConversation(message, bot, newConversation, [])
 
 
-def _manageConversation(message, bot, newConversation):
+def _manageConversation(message, bot, newConversation, userConversation):
     """
     Function to start or continue a conversation with the user and get responses from OpenAI
     
@@ -44,16 +44,17 @@ def _manageConversation(message, bot, newConversation):
         if newConversation:
             userConversation = []
             userConversation.append({"role": "system", "content": f"You are a bot that helps students to learn German. You need to have simple conversations, with short sentences, using only present tense. You will mainly use terms from the dictionary in the TermsList file, as these are the words the student knows. \nHere is the list of the terms: {german_words}"})
+            # print(f"system message: {userConversation}")
             assistantMessage = bot.reply_to(
                 message, f"Hallo, ich kann dir helfen zu Deutsch zu sprechen! 🇩🇪" + '\n' + "Remember you can end the conversation anytime by typig `end`"
             )
             userConversation.append({"role": "assistant", "content": assistantMessage.text})
             bot.register_next_step_handler(
-                message, lambda msg: _get_llm_response(msg, client, bot)
+                message, lambda msg: _get_llm_response(msg, userConversation, client, bot)
             )
         else:
             bot.register_next_step_handler(
-                message, lambda msg: _get_llm_response(msg, client, bot)
+                message, lambda msg: _get_llm_response(msg, userConversation, client, bot)
             )
         return userConversation
     except Exception as e:
@@ -61,7 +62,7 @@ def _manageConversation(message, bot, newConversation):
         return []
 
 
-def _get_llm_response(userMessage, client, bot):
+def _get_llm_response(userMessage, userConversation, client, bot):
     """
     Function to get responses from OpenAI and continue the conversation
 
@@ -81,4 +82,5 @@ def _get_llm_response(userMessage, client, bot):
             {"role": "assistant", "content": response.choices[0].message.content}
         )
         bot.reply_to(userMessage, response.choices[0].message.content)
-        _manageConversation(userMessage, bot, False)
+        _manageConversation(userMessage, bot, False, userConversation)
+        print(userConversation)
