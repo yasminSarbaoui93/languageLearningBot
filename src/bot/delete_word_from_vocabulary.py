ba065f09727d910ffeb68ae06804848b8d45f9be
yasminSarbaoui93 <yasmin.sarbaoui@gmail.com>
2025-01-18 22:19:58 +0100

    fix: delete_word to use text_in_base_language instead of performing language check locally

--- src/bot/delete_word_from_vocabulary.py
+++ src/bot/delete_word_from_vocabulary.py
@@ -3,7 +3,7 @@ This file contains the functions to be called by the bot to add a new word to th
 """
 import os
 from src.repository.vocabulary import delete_word, get_or_create_user
-from src.services.get_llm_response import translate_sentence_with_llm
+from src.services.get_llm_response import text_in_base_language
 
 def remove_word(user_message, bot):
     """
@@ -26,10 +26,8 @@ def _ask_for_word_to_be_deleted(user_message, bot):
     """
     user = get_or_create_user(str(user_message.from_user.id), user_message.from_user.username, user_message.from_user.first_name, user_message.from_user.last_name)
     base_language_code = user.base_language
-
-    bot_message = f"Type the word you want to remove from your vocabulary"
-    if base_language_code != "en":
-        bot_message = translate_sentence_with_llm(bot_message, base_language_code)
+    bot_message_english = f"Type the word you want to remove from your vocabulary"
+    bot_message = text_in_base_language(base_language_code, bot_message_english)
     bot.send_message(user_message.chat.id, bot_message)
     bot.register_next_step_handler(user_message, lambda user_message: _delete_word(user_message, bot, user))
 
@@ -49,17 +47,14 @@ def _delete_word(user_message, bot, user):
     try:
         words_deleted = delete_word(user_id, word_to_be_deleted)
         if words_deleted:
-            bot_message = f"The word <b>{word_to_be_deleted}</b> has been removed from your vocabulary"
-            if base_language_code != "en":
-                bot_message = translate_sentence_with_llm(bot_message, user.base_language)
+            bot_message_english = f"The word <b>{word_to_be_deleted}</b> has been removed from your vocabulary"
+            bot_message = text_in_base_language(base_language_code, bot_message_english)
         else:
-            bot_message = f"The word <b>{word_to_be_deleted}</b> could not be found in your vocabulary"
-            if base_language_code != "en":
-                bot_message = translate_sentence_with_llm(bot_message, user.base_language)
+            bot_message_english = f"The word <b>{word_to_be_deleted}</b> could not be found in your vocabulary"
+            bot_message = text_in_base_language(base_language_code, bot_message_english)
     except Exception as e:
         print(f"An error occurred: {e}")
-        bot_message = f"An error occurred while trying to remove the word <b>{word_to_be_deleted}</b> from your vocabulary."
-        if base_language_code != "en":
-            bot_message = translate_sentence_with_llm(bot_message, user.base_language)
+        bot_message_english = f"An error occurred while trying to remove the word <b>{word_to_be_deleted}</b> from your vocabulary."
+        bot_message = text_in_base_language(base_language_code, bot_message_english)
     
     bot.send_message(user_message.chat.id, bot_message, parse_mode='HTML')
