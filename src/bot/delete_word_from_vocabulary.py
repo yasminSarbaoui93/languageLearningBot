c47b9bfbc889d7bd0de3d2b8a145892a774fb7e0
yasminSarbaoui93 <yasmin.sarbaoui@gmail.com>
2025-01-19 18:58:47 +0100

    fix: delete_word to call send_bot_message function

--- src/bot/delete_word_from_vocabulary.py
+++ src/bot/delete_word_from_vocabulary.py
@@ -3,7 +3,7 @@ This file contains the functions to be called by the bot to add a new word to th
 """
 import os
 from src.repository.vocabulary import delete_word, get_or_create_user
-from services.llm_service import translate_to_language
+from src.bot.conversation_handling import send_bot_response
 
 def remove_word(user_message, bot):
     """
@@ -26,9 +26,8 @@ def _ask_for_word_to_be_deleted(user_message, bot):
     """
     user = get_or_create_user(str(user_message.from_user.id), user_message.from_user.username, user_message.from_user.first_name, user_message.from_user.last_name)
     base_language_code = user.base_language
-    bot_message_english = f"Type the word you want to remove from your vocabulary"
-    bot_message = translate_to_language(base_language_code, bot_message_english)
-    bot.send_message(user_message.chat.id, bot_message)
+    bot_message = f"Type the word you want to remove from your vocabulary"
+    send_bot_response(bot, user_message, [], base_language_code, bot_message)
     bot.register_next_step_handler(user_message, lambda user_message: _delete_word(user_message, bot, user))
 
 
@@ -41,20 +40,17 @@ def _delete_word(user_message, bot, user):
     bot: the bot object to send messages to the user
     """
     word_to_be_deleted = user_message.text
-    bot_message = ""
     user_id = user.id
     base_language_code = user.base_language
     try:
         words_deleted = delete_word(user_id, word_to_be_deleted)
         if words_deleted:
-            bot_message_english = f"The word <b>{word_to_be_deleted}</b> has been removed from your vocabulary"
-            bot_message = translate_to_language(base_language_code, bot_message_english)
+            bot_message = f"The word <b>{word_to_be_deleted}</b> has been removed from your vocabulary"
+            send_bot_response(bot, user_message, [], base_language_code, bot_message)
         else:
-            bot_message_english = f"The word <b>{word_to_be_deleted}</b> could not be found in your vocabulary"
-            bot_message = translate_to_language(base_language_code, bot_message_english)
+            bot_message = f"The word <b>{word_to_be_deleted}</b> could not be found in your vocabulary"
+            send_bot_response(bot, user_message, [], base_language_code, bot_message)
     except Exception as e:
         print(f"An error occurred: {e}")
-        bot_message_english = f"An error occurred while trying to remove the word <b>{word_to_be_deleted}</b> from your vocabulary."
-        bot_message = translate_to_language(base_language_code, bot_message_english)
-    
-    bot.send_message(user_message.chat.id, bot_message, parse_mode='HTML')
+        bot_message = f"An error occurred while trying to remove the word <b>{word_to_be_deleted}</b> from your vocabulary."
+        send_bot_response(bot, user_message, [], base_language_code, bot_message)    
